7b1c9ec5ac5aa81477cd4625e5359f2ed6e18024
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-01-12 13:40:24 -0300

    Function that makes a loading effect

--- client/script.js
+++ client/script.js
@@ -3,3 +3,17 @@ import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+  element.textContent = '';
+
+  loadInterval = setInterval(() =>{
+    element.textContent += '.';
+
+    if(element.textContent === '....'){
+      element.textContent = '';
+    }
+  }, 300)
+}
