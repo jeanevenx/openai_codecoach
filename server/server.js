9dee3228f6e1f1703cb91ea7394035d7beae47dc
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-01-12 14:03:06 -0300

    Added a get method

--- server/server.js
+++ server/server.js
@@ -17,3 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX',
+    })
+})
