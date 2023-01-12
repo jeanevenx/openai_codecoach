33959749e5b78187a7e6cff683ffb8cc1ff54d28
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-01-12 14:03:48 -0300

    Added the post method

--- server/server.js
+++ server/server.js
@@ -22,3 +22,28 @@ app.get('/', async (req, res) => {
         message: 'Hello from CodeX',
     })
 })
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+
+    } catch(error){
+        console.log(error);
+
+        res.status(500).send({error});
+    }
+})
